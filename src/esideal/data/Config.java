f640895dbdb8e0e9f3dbc4ed7736466c54be1203
Pedrosilva03 <pedroo.silva180@gmail.com>
2023-10-20 02:07:20 +0100

    Config da base de dados

---
+++ src/esideal/data/Config.java
@@ -0,0 +1,9 @@
+package esideal.data;
+
+public class Config {
+    static final String USERNAME = "root";
+    static final String PASSWORD = "";
+    private static final String DATABASE = "esideal";
+    private static final String DRIVER = "jdbc:mysql";
+    static final String URL = DRIVER + "://localhost:3306/" + DATABASE;
+}
